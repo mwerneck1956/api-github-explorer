7748e848196eaf4b64bcdd32f50a2db87e36e97f
matheus <werneckmatheus12@gmail.com>
2021-05-12 21:57:58 -0300

    Alterado config para testes

--- api-github-explorer/services/githubApi.js
+++ api-github-explorer/services/githubApi.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/',     
   headers : {
-    'Authorization' : process.env.GITHUB_TOKEN || ''
+    'Authorization' : 'token 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'
   }
 });
 
