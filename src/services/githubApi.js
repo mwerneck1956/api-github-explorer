dd367e34182283e175a13da9c8b5c82aa78868e7
matheus <werneckmatheus12@gmail.com>
2021-05-13 00:46:07 -0300

    Alterado token para variavel de ambiente

--- src/services/githubApi.js
+++ src/services/githubApi.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/',     
   headers : {
-    'Authorization' : 'token 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'
+    'Authorization' : process.env.GITHUB_TOKEN
   }
 });
 
